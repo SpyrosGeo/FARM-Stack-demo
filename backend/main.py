42cbf7979c89af2fbf9e3be2d88739afebc75a7c
SpyrosGeo <xarodasimba@gmail.com>
2021-08-14 04:06:58 +0300

    started adding basic routes

--- backend/main.py
+++ backend/main.py
@@ -1,10 +1,26 @@
 from typing import Optional
 from fastapi import FastAPI
-
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
+
+origins = ['https://localhost:3000']
+app.add_middleware(CORSMiddleware, allow_origins=origins,
+                   allow_credentials=True, allow_methods=["*"], allow_headers=["*"])
 
 
 @app.get("/")
 def read_root():
-    return {"Hello":"World"}
+    return {"Hello": "World"}
+
+
+@app.get("/api/todo")
+async def get_todo():
+    return 1
+
+@app.get("/api/todo{id}")
+async def get_todo_by_id(id):
+    return 1
+@app.post("/api/todo")
+async def post_todo():
+    return 1

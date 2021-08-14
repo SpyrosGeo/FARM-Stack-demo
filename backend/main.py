a00525dfc466d31dcd21bfc65633f69801ae7031
SpyrosGeo <xarodasimba@gmail.com>
2021-08-14 04:46:13 +0300

    finished building backend

--- backend/main.py
+++ backend/main.py
@@ -1,6 +1,11 @@
-from typing import Optional
-from fastapi import FastAPI
+# from typing import Optional
+
+from model import Todo
+from fastapi import FastAPI, HTTPException
 from fastapi.middleware.cors import CORSMiddleware
+from database import(
+    fetch_all_todos, fetch_one_todo, remove_todo, update_todo,create_todo,
+)
 
 app = FastAPI()
 
@@ -16,20 +21,38 @@ def read_root():
 
 @app.get("/api/todo")
 async def get_todo():
-    return 1
+    response = await fetch_all_todos()
+    return response
 
-@app.get("/api/todo{id}")
-async def get_todo_by_id(id):
-    return 1
 
-@app.post("/api/todo")
-async def post_todo(todo):
-    return 1
-    
-@app.put("/api/todo{id}")
-async def put_todo(id,data):
-    return 1
+@app.get("/api/todo{title}",response_model=Todo)
+async def get_todo_by_id(title):
+    response = await get_todo_by_id(title)
+    if response:
+        return response
+    raise HTTPException(404,f"Could not find title {title}")
 
-@app.delete("/api/todo{id}")
-async def delete_todo(id):
-    return 1
+
+@app.post("/api/todo",response_model=Todo)
+async def post_todo(todo:Todo):
+    response = await create_todo(todo.dict())
+    if response:
+        return "Succesfully deleted Todo item"
+    raise HTTPException(400,"Something went wrong")
+
+
+
+@app.put("/api/todo{title}",response_model=Todo)
+async def put_todo(title:str, desc:str):
+    response = await update_todo(title,desc)
+    if response:
+        return response
+    raise HTTPException(404,f"No todo item with the title {title}")
+
+
+@app.delete("/api/todo{title}")
+async def delete_todo(title):
+    response = await remove_todo(title)
+    if response:
+         return response
+    raise HTTPException(404,f"No todo item with the title {title}")
